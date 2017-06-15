f849886f63ca42dfb305f782d3867fdfeb1f925d
Mateusz <matstepien@outlook.com>
2017-06-15 18:24:28 +0200

    jQuery

---
+++ script.js
@@ -0,0 +1,21 @@
+$(function(){
+	var carouselList = $("#carousel ul");
+	function changeSlide() {
+		carouselList.animate({'marginLeft':-800}, 800, moveFirstSlide);
+	}
+	setInterval(changeSlide, 3000);
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+			lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	}
+	$('#next').on('click', changeSlide);
+	$('#previous').click(function() {
+		var firstItem = carouselList.find("li:first");
+			lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft:-800});
+		carouselList.animate({'marginLeft': 0 }, 800);
+	})
+});
